b9986f3b26587d471abb43009e985b3b4d30bb38
Edson Gaspar <114083090+EdsonGaspar@users.noreply.github.com>
2023-09-13 21:49:52 +0100

    Inicializando
    Inicializa o projeto, junto com o back-end, fazendo assim o start  do server juntamente.

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 import cors from "cors"
-import express, { response } from "express"
+import express from "express"
 
 const app = express()
 app.use(cors())
 
 /*Configurando a rota*/
-app.get("/summary", (request, response) => {
-  response.send("Hello")
+app.get("/summary/:id", (request, response) => {
+  response.send("ID do vídeo: " + request.params.id)
 })
 
 app.listen(3333, () => console.log("Server is running on port 3333"))
