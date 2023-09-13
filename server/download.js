2f0ca5b5324ad76e5dffb972ee6596cd66665d67
Edson Gaspar <114083090+EdsonGaspar@users.noreply.github.com>
2023-09-13 23:22:06 +0100

    Finalizando as funções do dwonload

--- server/download.js
+++ server/download.js
@@ -5,11 +5,21 @@ export const download = (videoid) => {
   const videoURL = "https://www.youtube.com/shorts/" + videoid
   console.log("Realizando download do vídeo: " + videoid)
 
-  ytdl(videoURL, { quality: "lowestaudio", filter: "audioonly" }).on(
-    "info",
-    (info) => {
+  ytdl(videoURL, { quality: "lowestaudio", filter: "audioonly" })
+    .on("info", (info) => {
       const seconds = info.formats[0].approxDurationMs / 1000
-      console.log(seconds)
-    }
-  )
+      //console.log(seconds)/*Duração do Vídeo*/
+      if (seconds > 60) {
+        throw new Error("A duração deste vídeo é maior que 60s")
+      }
+    })
+    .on("end", () => {
+      console.log("download do vídeo finalizado.")
+    })
+    .on("error", (error) => {
+      console.log(
+        "Não foi possivel fazer o dwonload do vídeo. Detalhes do error: ",
+        error
+      )
+    })
 }
