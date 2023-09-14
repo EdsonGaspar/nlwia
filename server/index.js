288fe317207aab48f2ccf30cc2ae12b5a6b25986
Edson Gaspar <114083090+EdsonGaspar@users.noreply.github.com>
2023-09-14 16:15:06 +0100

    Criando o arquivo summarize
    No arquivo summarise recebe um parametro e faz a simulação de uma transcrição do vídeo.
    E também criamos uma nova rota, ou seja a rota POST no arquivo index.js

--- server/index.js
+++ server/index.js
@@ -1,19 +1,28 @@
 import cors from "cors"
-import express from "express"
+import express, { request, response, text } from "express"
 
 import { download } from "./download.js"
 import { transcribe } from "./transcribe.js"
+import { summarize } from "./summarize.js"
 
 const app = express()
+app.use(express.json())
 app.use(cors())
 
-/*Configurando a rota*/
+/*Criação de uma rota com metodo GET*/
 app.get("/summary/:id", async (request, response) => {
   await download(request.params.id)
 
   const result = await transcribe()
 
-  response.json({ result })
+  return response.json({ result })
+})
+
+/*Criação de uma rota com método Post */
+app.post("/summary", async (request, response) => {
+  const result = await summarize(request.body.text)
+  
+  return response.json({ result })
 })
 
 app.listen(3333, () => console.log("Server is running on port 3333"))
